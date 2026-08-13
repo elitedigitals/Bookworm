58ac13cd64741dcd01465541134f9dc94eb0cc92
Eimiuhi Ehijie <eimiuhiehijie@gmail.com>
2026-08-13 23:43:54 +0100

    jjf

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,22 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import dns from "dns";
+
+dotenv.config({ quiet: true });
+
+// if (process.env.NODE_ENV === "development") {
+//   dns.setServers(["8.8.8.8", "8.8.4.4"]);
+// }
+
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Database connected ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI, {
+    });
+    console.log("MongoDB connected");
   } catch (error) {
-    console.log("Error connecting to database", error);
-    process.exit(1); // exit with failure
-  }
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  } 
 };
+
